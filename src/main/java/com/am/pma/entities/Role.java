ed7d54965ecd076f69e928ab1375dc8fbbdd3e2e
alexmonteil <47809218+alexmonteil@users.noreply.github.com>
2022-04-17 21:24:20 -0500

    Created Role Entity
    Created Role Entity for future authorization related features

---
+++ src/main/java/com/am/pma/entities/Role.java
@@ -0,0 +1,42 @@
+package com.am.pma.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "role_seq")
+    @SequenceGenerator(name = "role_seq", sequenceName = "role_seq", allocationSize = 1, initialValue = 1)
+    private long roleId;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    public Role() {}
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
