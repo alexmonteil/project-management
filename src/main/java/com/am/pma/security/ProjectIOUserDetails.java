914186d006c28436f3ea404c74a76e4d8a73e5f2
alexmonteil <47809218+alexmonteil@users.noreply.github.com>
2022-04-20 02:57:32 -0500

    Created ProjectIOUserDetails
    Created ProjectIOUserDetails class to implement spring security UserDetails
    and control what UserDetails are accessible through the UI

---
+++ src/main/java/com/am/pma/security/ProjectIOUserDetails.java
@@ -0,0 +1,63 @@
+package com.am.pma.security;
+
+import com.am.pma.entities.Role;
+import com.am.pma.entities.UserAccount;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+public class ProjectIOUserDetails implements UserDetails {
+
+    private final UserAccount userAccount;
+
+    public ProjectIOUserDetails(UserAccount userAccount) {
+        this.userAccount = userAccount;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<Role> roles = userAccount.getRoles();
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+        for (Role role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return userAccount.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return userAccount.getUserName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return userAccount.isEnabled();
+    }
+}
