999a33105199f1c738350a91420bd8bdde3e5502
alexmonteil <alex.monteil@outlook.com>
2022-06-21 20:23:21 -0500

    Updated configure method in SecurityConfiguration to implement HTTPS

--- src/main/java/com/am/pma/security/SecurityConfiguration.java
+++ src/main/java/com/am/pma/security/SecurityConfiguration.java
@@ -28,6 +28,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        http.requiresChannel()
+                .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+                .requiresSecure();
         http.authorizeRequests()
                 .antMatchers("/dashboard").hasAnyRole("ADMIN", "MANAGER")
                 .antMatchers("/projects").authenticated()
