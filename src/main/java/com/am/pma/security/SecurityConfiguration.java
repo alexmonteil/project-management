580faf501601a18b003b576f21d10e83d982c5ba
alexmonteil <alex.monteil@outlook.com>
2023-04-16 18:53:52 -0500

    trying new security config for railway

--- src/main/java/com/am/pma/security/SecurityConfiguration.java
+++ src/main/java/com/am/pma/security/SecurityConfiguration.java
@@ -29,7 +29,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.requiresChannel().anyRequest().requiresSecure();
+        http.requiresChannel().requestMatchers(r -> r.getHeader("X-Forward-Proto") != null).requiresSecure();
         http.authorizeRequests()
                 .antMatchers("/dashboard").hasAnyRole("ADMIN", "MANAGER")
                 .antMatchers("/projects").authenticated()
